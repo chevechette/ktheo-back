481304610267a6ef0be761ce8dd0c28a881eecf0
tuonetar <karina.martynava@gmail.com>
2022-04-21 12:28:27 +0200

    Project initialization

---
+++ src/test/java/fr/ktheo/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ktheo.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
